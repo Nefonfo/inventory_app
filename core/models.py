aef8338e274cf5e84624af3b95dc8c457693777b
Nefonfo <victorarmenta30@gmail.com>
2024-09-07 23:09:59 -0600

    inventory_app / core : Add BaseModel (for timestamps)

---
+++ core/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+
+# Create your models here.
+class BaseModel(models.Model):
+    class Meta:
+        abstract = True
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
